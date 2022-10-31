334411ad4f52f81f80215dc00a59f0a3e713a85c
vaidya0303 <vaidya0304@gmail.com>
2022-10-31 16:57:05 +0530

    constructor is used to pass the mood and return the desired m…

--- src/com/moodanalyser/MoodAnalyserMain.java
+++ src/com/moodanalyser/MoodAnalyserMain.java
@@ -1,25 +1,36 @@
 package com.moodanalyser;
 
 /**
- * UC1 :- Given a Message, ability to analyse and respond Happy or Sad Mood
- *       - Create MoodAnalyser Object
- *       - Call analyseMood function with message as parameter and return Happy or Sad Mood
+ * UC 2 :-
+ * Handle Exception if User Provides Invalid  Mood
+ * - Like NULL
  */
 
 /**
  * create a class name as MoodAnalyserMain
  */
 public class MoodAnalyserMain {
+    private String message;
+
     /**
-     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     * create a default constructor name as MoodAnalyserMain
      */
+
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
-            return "SAD";
-        else
+    public MoodAnalyserMain(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -8,13 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertSame;
  * create a class name as MoodAnalyserTest
  */
 public class MoodAnalyserTest {
+
+    @Test
+    public void given_SadMood_Should_Return_SAD() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertSame("SAD", mood);
+    }
+
     @Test
     public void given_AnyMood_Should_Return_HAPPY() {
-        /**
-         * create object for  MoodAnalyserMain class
-         */
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
         assertSame("HAPPY", mood);
     }
 }

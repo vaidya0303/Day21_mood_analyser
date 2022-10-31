7dbe874074175c39dbd5e7f076b00f3dcfa8a83e
vaidya0303 <vaidya0304@gmail.com>
2022-10-31 16:08:23 +0530

    When a sad mood is passed, should return SAD mood functionali…

--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -8,18 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertSame;
  * create a class name as MoodAnalyserTest
  */
 public class MoodAnalyserTest {
-
-    @Test
-    public void given_SadMood_Should_Return_SAD() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("SAD", mood);
-    }
-
     @Test
     public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
+        /**
+         * create a object for MoodAnalyserMain class
+         */
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        assertSame("SAD", mood);
     }
 }

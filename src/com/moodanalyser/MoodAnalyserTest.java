977dcfc3fc7a36038a366082443b054f01a7fa9b
vaidya0303 <vaidya0304@gmail.com>
2022-10-31 15:46:22 +0530

    Constructor is used to pass the mood and return the desired mo…

--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,25 @@
 package com.moodanalyser;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * create a class name as MoodAnalyserTest
+ */
 public class MoodAnalyserTest {
 
+    @Test
+    public void given_SadMood_Should_Return_SAD() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertSame("SAD", mood);
+    }
+
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Any Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertSame("HAPPY", mood);
+    }
 }
